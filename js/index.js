47444c840c0ed88fc970593e8499f34dcf894dae
sadia136310 <sadia1363@gmail.com>
2022-08-22 07:51:47 +0600

    player name updated

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const playerArray = [];
+
+function display(allPlayer) {
+    // console.log(allPlayer);
+    const body = document.getElementById("name-update");
+    body.innerHTML = "";
+
+    for (let i = 0; i < 5; i++) {
+        // console.log(playerArray[i].playerName);
+        const name = playerArray[i].playerName;
+
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+       <tr>${i + + 1}</tr>
+       <tr>${name}</tr>
+
+        `;
+        body.appendChild(tr)
+
+    }
+}
+function addToPlayer(element) {
+    // console.log(element.parentNode.parentNode.children[0])
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+    // console.log(playerName);
+
+    const playerObj = {
+        playerName: playerName
+    }
+
+    playerArray.push(playerObj)
+    console.log(playerArray);
+    console.log(playerArray.length);
+
+    display(playerArray);
+}
